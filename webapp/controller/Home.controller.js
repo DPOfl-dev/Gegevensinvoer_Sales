7ff90c63d09ab610b2bde1cb2a7aeb7670273aad
Jordi Hemelaer <jordi.hemelaer@flexso.com>
2019-08-14 12:54:00 +0000

    Deel 1 van de logica is overgezet naar de done()-functie.

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,54 +1,62 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
-	"use strict";
+			"sap/ui/core/mvc/Controller"
+		], function (Controller) {
+			"use strict";
 
-	return Controller.extend("com.Flexso.DPO.Gegevensinvoer_Sales.controller.Home", {
-		onInit: function () {
+			return Controller.extend("com.Flexso.DPO.Gegevensinvoer_Sales.controller.Home", {
+						onInit: function () {
 
-			// Velden voor elk template
+								// Velden voor de rol waarvoor de applicatie geschreven wordt.
 
-			var veldenTemplate1 = ["Customer", "Distr kanaal"];
-			var veldenTemplate2 = ["Sales organisatie"];
+								var veldenVerkoop = ["Customer", "Sales organisatie"];
 
-			// Velden voor de rol waarvoor de applicatie geschreven wordt.
-
-			var veldenVerkoop = ["Customer", "Sales organisatie"];
-
-			// GET-call van alle ideeën uit de IdeasSet
-
-			$.ajax({
-				type: "GET",
-				headers: {
-					"X-CSRF-Token": "Fetch",
-					"Authorization": "Basic aGVtZWxqbzpmeWQxVTRwOQ=="
-				},
-				url: "/sap/opu/odata/SAP/ZBC_DPO_WF_DATA_SRV/Ideas?$format=json",
-				success: function (res) {
-					for (var i = 0; i < res.d.results.length; i++) {
-
-						// Template geassocieerd aan idee opvragen.
-
-						var template = res.d.results[i].Template;
-
-						// Stappen in template opvragen.
-
-						if (template == 1) {
-							// Velden template 1 overlopen
-
-							for (var j = 0; j < veldenTemplate1.length; j++) {
+								// GET-call van alle ideeën uit de IdeasSet
 
 								$.ajax({
-									type: "GET",
-									headers: {
-										"X-CSRF-Token": "Fetch",
-										"Authorization": "Basic aGVtZWxqbzpmeWQxVTRwOQ=="
-									},
-									url: "/sap/opu/odata/SAP/ZBC_DPO_WF_DATA_SRV/Templates(Id=" + template + ",Field='" +
-										veldenTemplate1[j] +
-										"')?$format=json",
-									success: function (resTemplate1) {
-										console.log(resTemplate1);
+										type: "GET",
+										headers: {
+											"X-CSRF-Token": "Fetch",
+											"Authorization": "Basic aGVtZWxqbzpmeWQxVTRwOQ=="
+										},
+										url: "/sap/opu/odata/SAP/ZBC_DPO_WF_DATA_SRV/Ideas?$format=json",
+										success: function (res) {},
+										error: function (err) {
+											alert("Er is een fout opgetreden. Bekijk de console voor meer details");
+											console.log(err);
+										}
+									}).done(function (doneData) {
+
+											// Velden voor elk template
+
+											var veldenTemplate1 = ["Customer", "Distr kanaal"];
+											var veldenTemplate2 = ["Sales organisatie"];
+
+											console.log("Done-methode test");
+
+											for (var i = 0; i < doneData.d.results.length; i++) {
+
+												// Template geassocieerd aan idee opvragen.
+
+												var template = doneData.d.results[i].Template;
+
+												// Stappen in template opvragen.
+
+												if (template == 1) {
+													// Velden template 1 overlopen
+
+													for (var j = 0; j < veldenTemplate1.length; j++) {
+
+														$.ajax({
+																type: "GET",
+																headers: {
+																	"X-CSRF-Token": "Fetch",
+																	"Authorization": "Basic aGVtZWxqbzpmeWQxVTRwOQ=="
+																},
+																url: "/sap/opu/odata/SAP/ZBC_DPO_WF_DATA_SRV/Templates(Id=" + template + ",Field='" +
+																	veldenTemplate1[j] +
+																	"')?$format=json",
+																success: function (resTemplate1) {
+																	/*	console.log(resTemplate1);
 										if (resTemplate1.d.Role == "Verkoop") {
 
 											// Indien template een taak voor Sales bevat, kijken welk(e) veld(en) in welke stappen van het template ingevuld moeten worden.
@@ -97,22 +105,20 @@ sap.ui.define([
 								});
 							}
 						}
-					}
-				},
-				error: function (err) {
-					alert("Er is een fout opgetreden. Bekijk de console voor meer details");
-					console.log(err);
-				}
-			}).done(function (doneData) {
-				console.log("Done-methode test");
-			});
+					} */
+																});
 
-			// Opzoeken of de corresponderende record voor dat veld reeds een waarde bevat (elke keer doen)
+															// Opzoeken of de corresponderende record voor dat veld reeds een waarde bevat (elke keer doen)
 
-			// Indien dit nog geen waarde bevat: kijken in welke stap dit veld ingevuld moet worden
+															// Indien dit nog geen waarde bevat: kijken in welke stap dit veld ingevuld moet worden
 
-			// Invoer van gegevens mogelijk maken (POST-request naar de FieldsSet).
+															// Invoer van gegevens mogelijk maken (POST-request naar de FieldsSet).
 
-		}
-	});
-});
+															/*
+															Guid idee "Werkidee template 2" vanuit de done()-functie op te halen met volgende code: doneData.d.results[0].Guid
+															Guid idee "Werkidee template 1" vanuit de done()-functie op te halen met volgende code: doneData.d.results[1].Guid
+															*/
+
+														}
+													});
+											});
